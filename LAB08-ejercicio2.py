0013f23f12dd912ea1d98d3d5f3ba44ea7376ef1
Hugo Mijail Ramos Lujan <142697974+Laurenfox12@users.noreply.github.com>
2023-08-27 20:42:07 -0500

    Update LAB08-ejercicio2.py

--- LAB08-ejercicio2.py
+++ LAB08-ejercicio2.py
@@ -1,19 +1,1 @@
-"""
-El módulo 'sys' es un módulo incorporado en Python que proporciona acceso a algunas variables y funciones
-que interactúan con el intérprete de Python y su entorno. Se utiliza principalmente para realizar operaciones
-y acceder a información relacionada con el sistema y el entorno en el que se está ejecutando el programa Python.
 
-Algunas de las funciones y variables más comunes proporcionadas por el módulo 'sys' son:
-
-1. sys.argv: Lista de argumentos pasados al script de Python desde la línea de comandos.
-2. sys.exit([status]): Termina el programa y sale del intérprete de Python. El argumento opcional 'status'
-   puede ser un valor entero que se devuelve al sistema operativo.
-3. sys.version: Devuelve una cadena que contiene la versión de Python.
-4. sys.path: Lista de rutas en las que Python busca los módulos importados.
-5. sys.modules: Un diccionario que contiene los módulos que ya han sido importados.
-6. sys.stdin, sys.stdout, sys.stderr: Flujo estándar de entrada, salida y error respectivamente.
-
-En resumen, el módulo 'sys' es una herramienta útil para interactuar con el sistema operativo, obtener
-información sobre el entorno de ejecución y controlar el comportamiento de un programa Python.
-Se utiliza comúnmente en situaciones donde es necesario acceder a variables y funciones relacionadas con el sistema.
-"""
